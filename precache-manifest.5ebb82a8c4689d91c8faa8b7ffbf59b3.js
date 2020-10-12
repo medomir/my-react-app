7f21ef0064c6e7058a7dae1dd42f9c1713a3b7bc
medomirveliki <medomirveliki@gmail.com>
2020-10-12 10:40:54 +0200

    Updates

---
+++ precache-manifest.5ebb82a8c4689d91c8faa8b7ffbf59b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77eeb199023fd4d2426ca78b93160b46",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "adc195099dc4228e48ec",
+    "url": "/my-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdcab2610e626414cde3",
+    "url": "/my-react-app/static/js/2.067f0017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-app/static/js/2.067f0017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc195099dc4228e48ec",
+    "url": "/my-react-app/static/js/main.116fee12.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
